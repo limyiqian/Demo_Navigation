d660fe1d2f7588d6507a98cbb4888e74684dbeaa
19020125 <19020125@myrp.edu.sg>
2021-06-08 22:09:08 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19020125/demonavigation/NewActivity.java
@@ -0,0 +1,22 @@
+package sg.edu.rp.c346.id19020125.demonavigation;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
